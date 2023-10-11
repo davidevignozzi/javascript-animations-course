75b4ce71fb46e54605921b152cec8461999daabc
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-10-11 15:32:51 +0200

    :dizzy: SVG Animations => Notifications Animation

--- projects/SVGMicroAnimations/app.js
+++ projects/SVGMicroAnimations/app.js
@@ -3,7 +3,7 @@ import gsap from 'gsap';
 const tl = gsap.timeline({
   defaults: {
     duration: 0.35,
-    ease: 'power2.easeOut'
+    ease: 'power2.out'
   }
 });
 
@@ -30,4 +30,28 @@ home.addEventListener('click', () => {
     { y: 20, scale: 1.5, duration: 1, stagger: 0.25 }
   );
   gsap.fromTo('.right-feather', { x: 0 }, { x: 5 });
+});
+
+/**
+ * Notifications Animation
+ */
+gsap.set('.bell', { transformOrigin: 'top center' });
+gsap.set('.ringer', { transformOrigin: 'top center' });
+gsap.set('.wave', { autoAlpha: 0, transformOrigin: 'bottom' });
+notifications.addEventListener('click', () => {
+  gsap.fromTo(
+    '.bell',
+    { rotation: -5 },
+    { rotation: 0, duration: 2, ease: 'elastic.out(5, 0.2)' }
+  );
+  gsap.fromTo(
+    '.ringer',
+    { rotation: -3, x: 0.5 },
+    { rotation: 0, x: 0, duration: 1.25, ease: 'elastic.out(5, 0.2)' }
+  );
+  gsap.fromTo(
+    '.wave',
+    { scale: 0, autoAlpha: 1 },
+    { scale: 1.3, autoAlpha: 0, duration: 1 }
+  );
 });
