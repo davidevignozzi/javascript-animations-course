f1ca547b5c889cd017ddbeb72209677c45a32c62
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-10-12 12:47:22 +0200

    :dizzy: Interactive form animation => Elastic animation

---
+++ projects/interactiveFormAnimation/app.js
@@ -0,0 +1,84 @@
+import gsap from 'gsap';
+
+/**
+ * HTML Elements
+ */
+const containers = document.querySelectorAll('.input-container');
+const form = document.querySelector('form');
+
+const tl = gsap.timeline({ defaults: { duration: 1 } });
+
+/**
+ * SVG start = initial position
+ * SVG end = bended position
+ * Get those in figma
+ */
+const start =
+  'M0 0.999512C0 0.999512 60.5 0.999512 150 0.999512C239.5 0.999512 300 0.999512 300 0.999512';
+
+const end =
+  'M1 0.999512C1 0.999512 61.5 7.5 151 7.5C240.5 7.5 301 0.999512 301 0.999512';
+
+/**
+ * Elastic animation
+ */
+// Check if input is focused
+containers.forEach((container) => {
+  // Get the input, line and placeholder of the focused input
+  const input = container.querySelector('.input');
+  const line = container.querySelector('.elastic-line');
+  const placeholder = container.querySelector('.placeholder');
+
+  input.addEventListener('focus', () => {
+    // Check if there is any text in the input
+    if (!input.value) {
+      // Elastic effect
+      tl.fromTo(
+        line,
+        { attr: { d: start } },
+        { attr: { d: end }, ease: 'power2.out', duration: 0.75 }
+      );
+      tl.to(
+        line,
+        { attr: { d: start }, ease: 'elastic.out(3,0.5)' },
+        '<50%'
+      );
+
+      // Placeholder Shift
+      tl.to(
+        placeholder,
+        {
+          top: -15,
+          left: 0,
+          scale: 0.7,
+          duration: 0.5,
+          ease: 'power2.out'
+        },
+        '<15%'
+      );
+    }
+  });
+});
+// Revert when input is not focused
+form.addEventListener('click', () => {
+  containers.forEach((container) => {
+    // Get the input, line and placeholder of the focused input
+    const input = container.querySelector('.input');
+    const line = container.querySelector('.elastic-line');
+    const placeholder = container.querySelector('.placeholder');
+
+    // Check if the active element is not the input
+    if (document.activeElement !== input) {
+      // Check if there is any text in the input
+      if (!input.value) {
+        gsap.to(placeholder, {
+          top: 0,
+          left: 0,
+          scale: 1,
+          duration: 0.5,
+          ease: 'power2.out'
+        });
+      }
+    }
+  });
+});
