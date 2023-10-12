7ce1b21e93ea56bfbde28d07e7edea33606b9b21
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-10-12 15:48:58 +0200

    :sparkles: Interactive form => Animating Wave Goodbye

--- projects/interactiveFormAnimation/app.js
+++ projects/interactiveFormAnimation/app.js
@@ -6,6 +6,7 @@ import gsap from 'gsap';
 const containers = document.querySelectorAll('.input-container');
 const form = document.querySelector('form');
 const checkbox = document.querySelector('.checkbox');
+const button = document.querySelector('button');
 
 /**
  * Validate email
@@ -204,3 +205,29 @@ gsap.fromTo(
   { y: 0 },
   { y: -1, repeat: -1, yoyo: true, repeatDelay: 0.5, ease: 'power2.out' }
 );
+
+/**
+ * Goodbye Animation
+ */
+const tl3 = gsap.timeline({
+  defaults: { duration: 0.75, ease: 'power2.out' }
+});
+
+button.addEventListener('click', (e) => {
+  e.preventDefault();
+  tl.to('.contact-right, .contact-left', {
+    y: 30,
+    opacity: 0,
+    pointerEvents: 'none'
+  });
+  tl.to('form', { scale: 0.8 }, '<');
+  tl.fromTo('.submitted', { opacity: 0, y: 30 }, { opacity: 1, y: 0 });
+
+  // Hand Wave
+  gsap.set('#hand', { transformOrigin: 'left' });
+  gsap.fromTo(
+    '#hand',
+    { rotation: 0, y: 0 },
+    { rotation: -10, y: 2, ease: 'elastic(3,0.3)', duration: 2, delay: 1 }
+  );
+});
