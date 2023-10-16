88491bc8182d4ee76d7b61d552875eef4f63abbb
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-10-16 17:52:01 +0200

    :dizzy: Final Project => Parallax effect at the end

--- projects/iPhoneFinalProject/app.js
+++ projects/iPhoneFinalProject/app.js
@@ -151,3 +151,19 @@ tlVideo.fromTo(
   { autoAlpha: 1, duration: 0.5, stagger: 0.25 },
   '<'
 );
+
+/**
+ * Parallax Effect
+ *
+ */
+const tlParallax = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.sixth-page',
+    start: '-25%',
+    end: '50%',
+    scrub: true
+  }
+});
+tlParallax.fromTo('.photo-description', { y: 0 }, { y: -80 });
+tlParallax.fromTo('.portrait-container', { y: 0 }, { y: -80 }, '<');
+tlParallax.fromTo('.phone-video', { y: 0 }, { y: -200 }, '<');
